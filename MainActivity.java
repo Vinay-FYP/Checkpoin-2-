f17ff6e40d2ed069888e77ee901eeb7fcff4f94e
Vinay-FYP <87986297+Vinay-FYP@users.noreply.github.com>
2021-07-27 14:57:03 +0100

    Add files via upload
    Dashboard for home screen

--- MainActivity.java
+++ MainActivity.java
@@ -1,12 +1,8 @@
-package com.example.charts;
+package com.example.dashboard_modern;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-
-import com.github.mikephil.charting.charts.BarChart;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,26 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        findViewById(R.id.ButtonBarChart).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), BarChartActivity.class));
-            }
-        });
-
-        findViewById(R.id.ButtonPieChart).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), PieChartActivity.class));
-            }
-        });
-
-        findViewById(R.id.ButtonRadarChart).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),RadarChartActivity.class));
-            }
-        });
     }
 }
